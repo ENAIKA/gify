a08707bef33453426053687746850e8a87528cdc
ENAIKA <naikaesther5@gmail.com>
2020-04-11 23:53:53 +0300

    hide apikey

--- src/app/services/gify-service.service.ts
+++ src/app/services/gify-service.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class GifyServiceService {
-  
+  key=environment.apiKey
+  url=`https://api.giphy.com/v1/gifs/trending?api_key=${this.key}&limit=20`
   constructor(private httpClient:HttpClient) {}
     getData():Observable<any>{
-    return this.httpClient.get<any>("https://api.giphy.com/v1/gifs/trending?api_key=pjR3uS09gufHTypKbwNCzorocADyK0y2&limit=20")
+    return this.httpClient.get<any>(this.url)
    }
   
 }
