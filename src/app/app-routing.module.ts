93af993c6ccac49dc507f6a141b582f5995217d9
ENAIKA <naikaesther5@gmail.com>
2020-04-11 04:29:24 +0300

    add routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import {GifyComponent}from './gify/gify.component';
+import {NotFoundComponent}from './not-found/not-found.component'
+import {SearchGifsComponent} from './search-gifs/search-gifs.component';
+const routes: Routes = [
+  {path: 'trending_gif' ,component:GifyComponent},
+  { path:'trending_sticker', component:SearchGifsComponent},
+  { path: '', redirectTo:'/trending_gif', pathMatch:'full'},
+  { path:'**', component:NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
