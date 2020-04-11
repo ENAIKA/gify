03c5df198f3f0c8350e6ad6958aaa9dab4a6b9bb
ENAIKA <naikaesther5@gmail.com>
2020-04-11 04:22:46 +0300

    add modules to imports array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{FormsModule} from '@angular/forms';
+import{HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
+// import { from } from 'rxjs';
+import { GifyComponent } from './gify/gify.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SearchGifsComponent } from './search-gifs/search-gifs.component';
+import { GifSearchComponent } from './gif-search/gif-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GifyComponent,
+    NotFoundComponent,
+    NavBarComponent,
+    SearchGifsComponent,
+    GifSearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
